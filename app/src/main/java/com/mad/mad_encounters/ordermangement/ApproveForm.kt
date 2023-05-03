cd042c55b561bf1ba462ddbcd3c54efdac9fe427
Russell Peiris <ardpeiris@gmail.com>
2023-05-03 09:20:20 +0530

    modifications

--- app/src/main/java/com/mad/mad_encounters/ordermangement/ApproveForm.kt
+++ app/src/main/java/com/mad/mad_encounters/ordermangement/ApproveForm.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Spinner
 import android.widget.Toast
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -15,6 +16,8 @@ class ApproveForm : AppCompatActivity() {
 
     private lateinit var etOrderID : EditText
     private lateinit var etOrderName : EditText
+    private lateinit var etOrderCountry : EditText
+    private lateinit var etOrderMode : Spinner
     private lateinit var dbRef : DatabaseReference
     private lateinit var btnSubmit : Button
 
@@ -24,6 +27,8 @@ class ApproveForm : AppCompatActivity() {
 
         etOrderID = findViewById(R.id.editTextOrderID)
         etOrderName = findViewById(R.id.editTextOrderName)
+        etOrderCountry = findViewById(R.id.editTextCountry)
+        etOrderMode = findViewById(R.id.editTextMode)
         btnSubmit = findViewById(R.id.buttonSubmit)
 
         dbRef = FirebaseDatabase.getInstance().getReference("ApprovedOrders")
@@ -39,6 +44,8 @@ class ApproveForm : AppCompatActivity() {
     private fun saveAcceptedOrder() {
         val orderID = etOrderID.text.toString().trim()
         val orderName = etOrderName.text.toString().trim()
+        val orderCountry = etOrderCountry.text.toString().trim()
+        val orderMode = etOrderMode.selectedItem.toString().trim()
 
         if (orderID.isEmpty()) {
             etOrderID.error = "Please enter an order ID"
@@ -48,12 +55,28 @@ class ApproveForm : AppCompatActivity() {
             etOrderName.error = "Please enter an order name"
             return
         }
-
-        val rejectedOrder = OrderStatus(orderID, orderName, null, "Accepted")
-        dbRef.child(orderID).setValue(rejectedOrder).addOnCompleteListener {
+        if (orderCountry.isEmpty()) {
+            etOrderCountry.error = "Please enter an order country"
+            return
+        }
+        if (orderMode.isEmpty()) {
+            etOrderName.error = "Please enter an order mode"
+            return
+        }
+        //insert data into database
+        val acceptedOrder = OrderStatus(
+            orderID,
+            orderName,
+            orderCountry,
+            orderMode,
+            "Accepted"
+        )
+        dbRef.child(orderID).setValue(acceptedOrder).addOnCompleteListener {
             Toast.makeText(this, "Order Accepted", Toast.LENGTH_SHORT).show()
             etOrderID.setText("")
             etOrderName.setText("")
+            etOrderCountry.setText("")
+            etOrderMode.setSelection(0)
         }.addOnFailureListener{ err ->
             Toast.makeText(this, "Error: ${err.message}", Toast.LENGTH_SHORT).show()
         }
