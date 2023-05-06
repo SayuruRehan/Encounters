167548590de93e63d55df69bd30a8fee133a0a91
Russell Peiris <ardpeiris@gmail.com>
2023-05-07 00:12:04 +0530

    insert retrieve

--- app/src/main/java/com/mad/mad_encounters/inventorymanagement/activities/ViewInventory.kt
+++ app/src/main/java/com/mad/mad_encounters/inventorymanagement/activities/ViewInventory.kt
@@ -2,11 +2,51 @@ package com.mad.mad_encounters.inventorymanagement.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.Sampler.Value
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.*
 import com.mad.mad_encounters.R
+import com.mad.mad_encounters.inventorymanagement.adapter.ViewAdapter
+import com.mad.mad_encounters.inventorymanagement.model.InventoryItem
 
 class ViewInventory : AppCompatActivity() {
+
+    private lateinit var appRecyclerView: RecyclerView
+    private lateinit var viewItems: ArrayList<InventoryItem>
+    private lateinit var dbRef: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_inventory)
+
+        appRecyclerView = findViewById(R.id.recycler)
+        appRecyclerView.layoutManager = LinearLayoutManager(this)
+        appRecyclerView.setHasFixedSize(true)
+
+        viewItems = arrayListOf<InventoryItem>()
+        getInventoryList()
+    }
+    private fun getInventoryList(){
+        appRecyclerView.visibility = View.GONE
+        dbRef = FirebaseDatabase.getInstance().getReference("InventoryItems")
+        dbRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                viewItems.clear()
+                if (snapshot.exists()) {
+                    for (item in snapshot.children) {
+                        val inventoryItem = item.getValue(InventoryItem::class.java)
+                        viewItems.add(inventoryItem!!)
+                    }
+                    appRecyclerView.adapter = ViewAdapter(viewItems)
+                    appRecyclerView.visibility = View.VISIBLE
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@ViewInventory, "Error: ${error.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
--- app/src/main/java/com/mad/mad_encounters/inventorymanagement/adapter/ViewAdapter.kt
+++ app/src/main/java/com/mad/mad_encounters/inventorymanagement/adapter/ViewAdapter.kt
@@ -3,6 +3,8 @@ package com.mad.mad_encounters.inventorymanagement.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.mad.mad_encounters.R
 import com.mad.mad_encounters.inventorymanagement.model.InventoryItem
@@ -16,10 +18,10 @@ class ViewAdapter(private val view : ArrayList<InventoryItem>) : RecyclerView.Ad
     override fun onBindViewHolder(holder: ViewViewHolder, position: Int) {
         val currentItem = view[position]
         holder.cusId.text = currentItem.cusId
-        holder.cusName.text = currentItem.cusName
+//        holder.cusName.text = currentItem.cusName
         holder.item.text = currentItem.item
         holder.quantity.text = currentItem.quantity
-        holder.cusCountry.text = currentItem.cusCountry
+//        holder.cusCountry.text = currentItem.cusCountry
     }
 
     override fun getItemCount(): Int {
@@ -27,11 +29,11 @@ class ViewAdapter(private val view : ArrayList<InventoryItem>) : RecyclerView.Ad
     }
 
     class ViewViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val cusId = itemView.findViewById<android.widget.TextView>(R.id.cusId)
-        val cusName = itemView.findViewById<android.widget.TextView>(R.id.cusName)
-        val item = itemView.findViewById<android.widget.TextView>(R.id.item)
-        val quantity = itemView.findViewById<android.widget.TextView>(R.id.quantity)
-        val cusCountry = itemView.findViewById<android.widget.TextView>(R.id.cusCountry)
+        val cusId = itemView.findViewById<TextView>(R.id.editTextTextPersonName6)
+//        val cusName = itemView.findViewById<android.widget.TextView>(R.id.cusName)
+        val item = itemView.findViewById<TextView>(R.id.textView2)
+        val quantity = itemView.findViewById<TextView>(R.id.tvQty)
+//        val cusCountry = itemView.findViewById<android.widget.TextView>(R.id.cusCountry)
         val viewBtn = itemView.findViewById<android.widget.Button>(R.id.viewBtn)
 
         init {
@@ -40,7 +42,9 @@ class ViewAdapter(private val view : ArrayList<InventoryItem>) : RecyclerView.Ad
                 if (adapterPosition != RecyclerView.NO_POSITION) {
                     // Call the view function on the adapter with the current position
 //                    (itemView.context as com.mad.mad_encounters.inventorymanagement.activities.ViewInventory).viewOrder(adapterPosition)
+                    Toast.makeText(itemView.context, "Not implemented", Toast.LENGTH_SHORT).show()
                 }
+
             }
         }
 
