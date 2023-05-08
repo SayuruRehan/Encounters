cf015a9f491aae1a3ba7d9f19ac654ec051b97d7
shanuka27 <shanukayasiru27@gmail.com>
2023-05-08 13:30:13 +0530

    update and delete

--- app/src/main/java/com/mad/mad_encounters/inventorymanagement/activities/InventoryDetailsActivity.kt
+++ app/src/main/java/com/mad/mad_encounters/inventorymanagement/activities/InventoryDetailsActivity.kt
@@ -1,10 +1,16 @@
 package com.mad.mad_encounters.inventorymanagement.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.database.FirebaseDatabase
 import com.mad.mad_encounters.R
+import com.mad.mad_encounters.inventorymanagement.model.InventoryItem
 
 class InventoryDetailsActivity : AppCompatActivity() {
 
@@ -21,7 +27,22 @@ class InventoryDetailsActivity : AppCompatActivity() {
 
         initView()
         setValuesToViews()
+
+        btnUpdate.setOnClickListener {
+            openUpdateDialog(
+                intent.getStringExtra("InvId").toString(),
+                intent.getStringExtra("InvName").toString()
+            )
+        }
+
+        btnDelete.setOnClickListener {
+            deleteRecord(
+                intent.getStringExtra("InvId").toString()
+            )
+        }
     }
+
+
 
     private fun initView() {
         tvEmpId = findViewById(R.id.tvEmpId)
@@ -38,5 +59,67 @@ class InventoryDetailsActivity : AppCompatActivity() {
         tvEmpName.text = intent.getStringExtra("InvName")
         tvEmpAge.text = intent.getStringExtra("InvCon")
         tvEmpSalary.text = intent.getStringExtra("InvItem")
+    }
+
+    private fun openUpdateDialog(InvId:String,InvName:String){
+        val mDialog = AlertDialog.Builder(this)
+        val inflater = layoutInflater
+        val mDialogView = inflater.inflate(R.layout.activity_update_inventory, null)
+
+        mDialog.setView(mDialogView)
+
+        val etEmpName = mDialogView.findViewById<EditText>(R.id.etEmpName)
+        val etEmpAge = mDialogView.findViewById<EditText>(R.id.etEmpAge)
+        val etEmpSalary = mDialogView.findViewById<EditText>(R.id.etEmpSalary)
+        val btnUpdateData = mDialogView.findViewById<Button>(R.id.btnUpdateData)
+
+        etEmpName.setText(intent.getStringExtra("InvName").toString())
+        etEmpAge.setText(intent.getStringExtra("InvCon").toString())
+        etEmpSalary.setText(intent.getStringExtra("InvItem").toString())
+
+        mDialog.setTitle("Updating $InvName Record")
+
+        val alertDialog = mDialog.create()
+        alertDialog.show()
+
+        btnUpdateData.setOnClickListener{
+            updateInvData(
+                InvId,
+                etEmpName.text.toString(),
+                etEmpAge.text.toString(),
+                etEmpSalary.text.toString()
+            )
+            Toast.makeText(applicationContext, "Employee Data Updated", Toast.LENGTH_LONG).show()
+
+            tvEmpName.text = etEmpName.text.toString()
+            tvEmpAge.text = etEmpAge.text.toString()
+            tvEmpSalary.text = etEmpSalary.text.toString()
+
+            alertDialog.dismiss()
+        }
+    }
+    private fun updateInvData(id: String, name: String, age: String, salary:String ){
+
+        val dbRef = FirebaseDatabase.getInstance().getReference("InventoryItems").child(id)
+        val InvInfo = InventoryItem(id, name, age, salary)
+        dbRef.setValue(InvInfo)
+
+    }
+
+    private fun deleteRecord(
+        id: String
+    ){
+        val dbRef = FirebaseDatabase.getInstance().getReference("InventoryItems").child(id)
+        val mTask = dbRef.removeValue()
+
+        mTask.addOnSuccessListener {
+            Toast.makeText(this, "Employee data deleted", Toast.LENGTH_LONG).show()
+
+            val intent = Intent(this, ViewInventory::class.java)
+            finish()
+            startActivity(intent)
+        }.addOnFailureListener{ error ->
+            Toast.makeText(this, "Deleting Err ${error.message}", Toast.LENGTH_LONG).show()
+        }
     }
 }
