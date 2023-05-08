490a633a10655529567cb0d69c1dcce0e48868aa
KavinduGithmin <kgithmin@gmail.com>
2023-05-08 21:04:26 +0530

    updated add inventory part

---
+++ app/src/main/java/com/mad/mad_encounters/AddInventory.kt
@@ -0,0 +1,14 @@
+//IT21267222
+
+package com.mad.mad_encounters
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddInventory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_inventory)
+    }
+}
+
