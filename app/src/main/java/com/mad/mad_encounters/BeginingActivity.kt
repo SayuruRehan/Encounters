a9393d9ab6675a20f8fc3966bf13709eb2e21666
shanuka27 <shanukayasiru27@gmail.com>
2023-05-13 21:38:34 +0530

    add common navigations

--- app/src/main/java/com/mad/mad_encounters/BeginingActivity.kt
+++ app/src/main/java/com/mad/mad_encounters/BeginingActivity.kt
@@ -1,11 +1,18 @@
 package com.mad.mad_encounters
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class BeginingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.begining)
+    }
+
+    fun navigatefirstpage(view: View) {
+        val intent = Intent(this, FirstpageActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/mad/mad_encounters/FirstpageActivity.kt
+++ app/src/main/java/com/mad/mad_encounters/FirstpageActivity.kt
@@ -1,11 +1,24 @@
 package com.mad.mad_encounters
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.mad.mad_encounters.ordermangement.activities.OrderManagementMain
+import com.mad.mad_encounters.usermanagement.activity.Home
 
 class FirstpageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.firstpage)
+    }
+    fun navigateuserpage(view: View) {
+        val intent = Intent(this, Home::class.java)
+        startActivity(intent)
+    }
+
+    fun navigateadminpage(view: View) {
+        val intent = Intent(this, OrderManagementMain::class.java)
+        startActivity(intent)
     }
 }
